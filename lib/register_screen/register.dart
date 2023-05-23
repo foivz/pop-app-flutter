719e930dd4c3ec8e7b1eed870463fa1c99d44b52
Mislav Matijević <mislav.mim@gmail.com>
2023-05-23 22:29:29 +0200

    Enable switching of register screens. There's a couple of register screens. The things are now in place to support interchanging these screens. (#1)

--- lib/register_screen/register.dart
+++ lib/register_screen/register.dart
@@ -4,49 +4,79 @@ import 'package:pop_app/login_screen/custom_textformfield_widget.dart';
 import 'package:pop_app/myconstants.dart';
 
 class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({super.key});
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController surnameController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  RegisterScreen({super.key});
   @override
   State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
-  TextEditingController firstNameController = TextEditingController();
-  TextEditingController surnameController = TextEditingController();
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  int _currentStep = 0;
+  List<Widget> registerScreens = [];
+
+  void showNextRegisterScreen() {
+    setState(() {
+      _currentStep++;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    registerScreens.add(FirstRegisterScreen(widget, showNextRegisterScreen));
+    registerScreens.add(const Placeholder());
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register yourself'),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CustomTextFormField(
-                inputLabel: "First Name",
-                textEditingController: firstNameController,
-                autoFocus: true,
-              ),
-              const SizedBox(height: MyConstants.formInputSpacer),
-              CustomTextFormField(
-                inputLabel: "Surname",
-                textEditingController: surnameController,
-                obscureText: true,
-              ),
-              const SizedBox(height: MyConstants.formInputSpacer * 1.5),
-              FormSubmitButton(
-                buttonText: 'Next',
-                onPressed: () => {},
-                type: FormSubmitButtonType.RED_FILL,
-              ),
-            ],
+    return WillPopScope(
+        onWillPop: () async {
+          setState(() {
+            _currentStep--;
+          });
+          return false;
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Register yourself'),
+            ),
+            body: Center(child: registerScreens[_currentStep])));
+  }
+}
+
+class FirstRegisterScreen extends StatelessWidget {
+  final RegisterScreen widget;
+  final Function proceedToNextPage;
+  const FirstRegisterScreen(this.widget, this.proceedToNextPage, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: widget._formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CustomTextFormField(
+            inputLabel: "First Name",
+            textEditingController: widget.firstNameController,
+            autoFocus: true,
           ),
-        ),
+          const SizedBox(height: MyConstants.formInputSpacer),
+          CustomTextFormField(
+            inputLabel: "Surname",
+            textEditingController: widget.surnameController,
+            obscureText: true,
+          ),
+          const SizedBox(height: MyConstants.formInputSpacer * 1.5),
+          FormSubmitButton(
+            buttonText: 'Next',
+            onPressed: () => proceedToNextPage(),
+            type: FormSubmitButtonType.RED_FILL,
+          )
+        ],
       ),
     );
   }
