a0b66489460d46262dcdcff9d89c46d00bff609b
Mislav Matijević <mmatijevi@foi.hr>
2023-05-26 11:31:10 +0200

    Prepare code for user registration. In order to enable registration on the fifth screen, fifth screen is now a stateful widget. As such, it is going to handle the registration process itself. (#1)

--- lib/register_screen/register_screen_5.dart
+++ lib/register_screen/register_screen_5.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:pop_app/login_screen/custom_elevatedbutton_widget.dart';
 import 'package:pop_app/myconstants.dart';
-import 'package:pop_app/register_screen/register.dart';
 
-class FifthRegisterScreen extends StatelessWidget {
+class FifthRegisterScreen extends StatefulWidget {
   const FifthRegisterScreen({super.key});
+
+  @override
+  State<FifthRegisterScreen> createState() => _FifthRegisterScreenState();
+}
+
+class _FifthRegisterScreenState extends State<FifthRegisterScreen> {
+  bool isLoading = true;
+
+  final Widget loaderContent = const CircularProgressIndicator();
+
+  final Widget registrationSuccessfulContent = const Card(
+    child: Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Text(
+        "A professor has to confirm your registration.\n\n"
+        "After your account is confirmed, you will gain access to the rest of the application.",
+        style: TextStyle(
+            color: MyConstants.red,
+            fontSize: 16,
+            fontFamily: "RobotoMono-Regular"),
+      ),
+    ),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,23 +39,10 @@ class FifthRegisterScreen extends StatelessWidget {
         onPressed: () => Navigator.pop(context),
         child: const Icon(Icons.check),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Center(
-          child: Card(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                "A professor has to confirm your registration.\n\n"
-                "After your account is confirmed, you will gain access to the rest of the application.",
-                style: TextStyle(
-                    color: MyConstants.red,
-                    fontSize: 16,
-                    fontFamily: "RobotoMono-Regular"),
-              ),
-            ),
-          ),
-        ),
+            child: isLoading ? loaderContent : registrationSuccessfulContent),
       ),
     );
   }
