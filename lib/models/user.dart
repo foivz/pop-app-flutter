0767ca9a765bdc84f58c423c31fca360f571b418
Mislav Matijević <mislav.mim@gmail.com>
2023-05-27 03:24:42 +0200

    Add UserRole class. Needed to easily manage roles. (#1)

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -12,6 +12,9 @@ class User {
 
   User(this.firstName, this.lastName, this.username, this.email, this.password);
 
-  User.full(this.firstName, this.lastName, this.username, this.email, this.password, this.role,
-      this.storeName);
+class UserRole {
+  late int roleId;
+  late String roleName;
+
+  UserRole(this.roleId, this.roleName);
 }
