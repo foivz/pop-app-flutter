a193bea5a7a32d0c5b57c50b63d86c748ab63dad
Mislav Matijević <mislav.mim@gmail.com>
2023-06-03 23:34:20 +0200

    Add wallet screen. Uses stateful widget for displaying a loader while fetching balance data. (#6)

---
+++ lib/main_menu_screen/wallet_screen/wallet_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WalletScreen extends StatefulWidget {
+  const WalletScreen({super.key});
+
+  @override
+  State<WalletScreen> createState() => _WalletScreenState();
+}
+
+class _WalletScreenState extends State<WalletScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
