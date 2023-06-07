b8acccf63d43c3100ddbefe1581e3a72c2aee4da
Mislav Matijević <mislav.mim@gmail.com>
2023-06-07 23:17:27 +0200

    Enable displaying of fetched invoices for user. Role model for the implementation was the products tab. (#7)

--- lib/main_menu_screen/invoices_screen/invoices_screen.dart
+++ lib/main_menu_screen/invoices_screen/invoices_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pop_app/api_requests.dart';
+import 'package:pop_app/main_menu_screen/invoices_screen/invoice_card.dart';
+import 'package:pop_app/models/user.dart';
+import 'package:pop_app/myconstants.dart';
 
 class InvoicesScreen extends StatefulWidget {
   const InvoicesScreen({super.key});
@@ -10,6 +14,55 @@ class InvoicesScreen extends StatefulWidget {
 class _InvoicesScreenState extends State<InvoicesScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: FutureBuilder(
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data!.isNotEmpty) {
+                return ListView.separated(
+                  itemCount: snapshot.data!.length,
+                  separatorBuilder: (_, __) => const Divider(indent: 3, endIndent: 3, thickness: 0),
+                  padding: const EdgeInsets.all(5),
+                  itemBuilder: (context, index) {
+                    return InvoiceCard(
+                      index: index,
+                      invoice: snapshot.data![index],
+                    );
+                  },
+                );
+              } else {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "No invoices",
+                        style: TextStyle(
+                            color: MyConstants.red, fontSize: 50, fontWeight: FontWeight.bold),
+                      ),
+                      const Text(
+                        "It's empty in here... 👀\nStart trading and then come back!",
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 20,
+                        ),
+                      ),
+                      BackButton(
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.resolveWith((states) => MyConstants.red))),
+                    ],
+                  ),
+                );
+              }
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+          future: ApiRequestManager.getAllInvoices(),
+        ),
+      ),
+    );
   }
 }
