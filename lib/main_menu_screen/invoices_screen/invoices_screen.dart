a3f1fa284ad301bca536947538fdc27b40f9e41a
Mislav Matijević <mislav.mim@gmail.com>
2023-06-07 21:11:01 +0200

    Add prelimenary invoices screen. Opens in both buyer and seller menus. Currently just a placeholder. (#7)

---
+++ lib/main_menu_screen/invoices_screen/invoices_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InvoicesScreen extends StatefulWidget {
+  const InvoicesScreen({super.key});
+
+  @override
+  State<InvoicesScreen> createState() => _InvoicesScreenState();
+}
+
+class _InvoicesScreenState extends State<InvoicesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
