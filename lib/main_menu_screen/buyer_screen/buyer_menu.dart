4db3dcf84225c8879e43c3e53f47d110e39917de
Mislav Matijević <mislav.mim@gmail.com>
2023-06-08 18:37:03 +0200

    Changed the buying option. Previously, only the dialog was opened with QR and NFC options. Since the implementation of the menu screen mixin, it is much better to open an entire new screen on BUY option selection. The NFC option was ditched so now only QR is avaliable. (#25)

--- lib/main_menu_screen/buyer_screen/buyer_menu.dart
+++ lib/main_menu_screen/buyer_screen/buyer_menu.dart
@@ -1,4 +1,4 @@
-import 'package:pop_app/main_menu_screen/buyer_screen/buying_dialog/buying_dialog.dart';
+import 'package:pop_app/main_menu_screen/buyer_screen/buying_screen/buying_screen.dart';
 import 'package:pop_app/main_menu_screen/menu_screen_mixin.dart';
 import 'package:pop_app/models/user.dart';
 import 'package:pop_app/myconstants.dart';
@@ -25,7 +25,7 @@ class _BuyerMenuState extends State<BuyerMenu> with MenuScreenMixin {
   Widget build(BuildContext context) {
     return user != null
         ? generateMenu(
-            context: context, nameOfCustomOption: "Buy", customOptionScreen: const BuyerDialog())
+            context: context, nameOfCustomOption: "Buy", customOptionScreen: const BuyingScreen())
         : const CircularProgressIndicator();
   }
 }
