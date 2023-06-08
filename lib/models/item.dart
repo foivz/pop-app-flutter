0f6c1b47962a80de28a04c58a9d5a00e344246f3
Mislav Matijević <mislav.mim@gmail.com>
2023-06-08 23:02:39 +0200

    Fix Item's id to be String instead of int. (#10)

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,8 +1,8 @@
 abstract class Item {
-  int id;
+  String id;
   late String title;
   late String description;
   late String image;
 
-  Item({this.id = 0, required this.title, required this.description, required this.image});
+  Item({this.id = "0", required this.title, required this.description, required this.image});
 }
