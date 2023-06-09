e5c91fec080856a69d5a608c7da97f17a485233c
Mislav Matijević <mislav.mim@gmail.com>
2023-06-09 02:06:50 +0200

    Show bottom nav bar on sell items screen. Enables adding discount and viewing total before commiting the sale. (#10)

--- lib/main_menu_screen/seller_screen/sales_menu/sell_items_screen.dart
+++ lib/main_menu_screen/seller_screen/sales_menu/sell_items_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pop_app/main_menu_screen/seller_screen/sales_menu/item_card.dart';
 import 'package:pop_app/models/item.dart';
+import 'package:pop_app/myconstants.dart';
 
 class SellItemsScreen extends StatefulWidget {
   final List<Item> selectedItems;
@@ -15,10 +16,15 @@ class _SellItemsScreenState extends State<SellItemsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Invoice generation")),
+      bottomNavigationBar: const SellContent(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.sell),
+        onPressed: () {},
+      ),
       body: ListView.separated(
         itemCount: widget.selectedItems.length,
         shrinkWrap: true,
-        clipBehavior: Clip.hardEdge,
+        clipBehavior: Clip.none,
         separatorBuilder: (context, index) => const Divider(
           indent: 3,
           endIndent: 3,
@@ -33,6 +39,116 @@ class _SellItemsScreenState extends State<SellItemsScreen> {
             onAmountChange: (newAmount) {},
           );
         },
+      ),
+    );
+  }
+}
+
+class SellContent extends StatefulWidget {
+  const SellContent({super.key});
+
+  @override
+  State<SellContent> createState() => _SellContentState();
+}
+
+class _SellContentState extends State<SellContent> {
+  GlobalKey<FormFieldState>? discountInputKey = GlobalKey();
+  TextEditingController discountInputCont = TextEditingController(text: "0");
+  double contentHeight = 120;
+
+  void returnToNormalSize() {
+    FocusManager.instance.primaryFocus?.unfocus();
+    setState(() {
+      contentHeight = 120;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      width: 100,
+      height: contentHeight,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.elliptical(20, 10),
+          topRight: Radius.elliptical(20, 10),
+        ),
+        color: MyConstants.red,
+      ),
+      duration: const Duration(milliseconds: 200),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16.0, top: 16.0, right: 120, bottom: 16.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Form(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "DISCOUNT (%):",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  color: MyConstants.textfieldBackground,
+                  width: 75,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Stack(children: [
+                    TextFormField(
+                      key: discountInputKey,
+                      controller: discountInputCont,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        floatingLabelStyle: MaterialStateTextStyle.resolveWith(
+                          (states) => const TextStyle(
+                            color: MyConstants.red,
+                            fontSize: 15,
+                          ),
+                        ),
+                        focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: MyConstants.accentColor),
+                        ),
+                        contentPadding: const EdgeInsets.fromLTRB(0, 5, 5, 5),
+                      ),
+                      textInputAction: TextInputAction.done,
+                      onTap: () => setState(() {
+                        contentHeight = 500;
+                      }),
+                      onEditingComplete: () => returnToNormalSize(),
+                      onTapOutside: (event) {
+                        returnToNormalSize();
+                      },
+                    ),
+                  ]),
+                )
+              ],
+            ),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "TOTAL:",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                "50",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          )
+        ]),
       ),
     );
   }
