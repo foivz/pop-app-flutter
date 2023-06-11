58f7b91045afecc130a43ebda201f5b209f2fbed
Mislav Matijević <mislav.mim@gmail.com>
2023-06-11 17:48:31 +0200

    Prepare products notifier. This notifier takes care of the products list. It broadcasts all changes that may affect it. This is not the best solution since the provider wraps the entire app, but since package_creation_2 opens as a "bottom sheet", this is the only thing that works. (#34)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:pop_app/login_screen/login_screen.dart';
+import 'package:pop_app/models/available_products.dart';
 import 'package:pop_app/themes.dart';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +14,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pop app',
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme(context),
-      home: const BaseLoginScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => AvailableProducts(),
+      child: MaterialApp(
+        title: 'Pop app',
+        debugShowCheckedModeBanner: false,
+        theme: lightTheme(context),
+        home: const BaseLoginScreen(),
+      ),
     );
   }
 }
---
+++ lib/models/available_products.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:pop_app/models/item.dart';
+
+class AvailableProducts extends ChangeNotifier {
+  final List<Item> _items = [];
+
+  void addAll(List<Item> items) {
+    _items.addAll(items);
+    notifyListeners();
+  }
+
+  void removeAll() {
+    _items.clear();
+    notifyListeners();
+  }
+
+  void removeById(String id) {
+    _items.removeWhere((element) => element.id == id);
+  }
+
+  void add(Item item) {
+    _items.add(item);
+  }
+
+  void edit(Item item) {
+    removeById(item.id);
+    _items.add(item);
+  }
+
+  int getLength() => _items.length;
+
+  Item getElement(int index) => _items[index];
+
+  List<Item> getSelectedForPackaging() =>
+      _items.where((element) => element.selectedForPackaging > 0).toList();
+
+  List<Item> getSelectedForSelling() =>
+      _items.where((element) => element.selectedForSelling > 0).toList();
+}
